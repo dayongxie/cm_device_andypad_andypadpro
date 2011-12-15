79bededf79f7d9b1f51a0808d90acf9ad8617065
Kevin Armstrong <kja@blueyonder.co.uk>
2011-12-15 22:42:22 +0000

    first cm9 commit

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,11 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
-
-add_lunch_combo cm_andypadpro-userdebug
+include $(call all-makefiles-under,$(LOCAL_PATH))
